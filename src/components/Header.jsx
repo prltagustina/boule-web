8b72671512e48e98abf605cb7184e5f7eeb77c05
prltagustina <prltagustina@gmail.com>
2024-06-30 14:05:17 -0300

    change text

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import Button from "./UI/Button";
+import logoImg from "../assets/logo.jpg";
+export default function Header() {
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logoImg} alt="A restaurant" />
+        <h1>Boule El Pan</h1>
+      </div>
+      <nav>
+        <Button textOnly>Mi pedido (0)</Button>
+      </nav>
+    </header>
+  );
+}
