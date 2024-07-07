d6a109e243ca48e1d1cdbb5a48eb9618ff873725
prltagustina <prltagustina@gmail.com>
2024-07-06 21:10:14 -0300

    new config

---
+++ boule-web-main/src/components/MealItem.jsx
@@ -0,0 +1,56 @@
+<<<<<<< HEAD
+import { currencyFormatter } from "../util/formatting"
+import Button from "./UI/Button"
+// {}
+export default function MealItem({meal}) {
+    return (
+        <li className="meal-item">
+        <article>
+            <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
+        <div>
+            <h3>{meal.name}</h3>
+            <p className="meal-item-price">
+                {currencyFormatter.format(meal.price)}
+            </p>
+            <p className="meal-item-description">{meal.description}</p>
+        </div>
+        <p className="meal-item-actions">
+            <Button>Añadir al Carrito</Button>
+        </p>
+        </article>
+    </li>
+    )
+} 
+=======
+import { useContext } from 'react';
+
+import { currencyFormatter } from '../util/formatting.js';
+import Button from './UI/Button.jsx';
+import CartContext from '../store/CartContext.jsx';
+
+export default function MealItem({ meal }) {
+  const cartCtx = useContext(CartContext);
+
+  function handleAddMealToCart() {
+    cartCtx.addItem(meal);
+  }
+
+  return (
+    <li className="meal-item">
+      <article>
+        <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
+        <div>
+          <h3>{meal.name}</h3>
+          <p className="meal-item-price">
+            {currencyFormatter.format(meal.price)}
+          </p>
+          <p className="meal-item-description">{meal.description}</p>
+        </div>
+        <p className="meal-item-actions">
+          <Button onClick={handleAddMealToCart}>Add to Cart</Button>
+        </p>
+      </article>
+    </li>
+  );
+}
+>>>>>>> 5ca97e896659f01ace48d1e53c4f3ae89f4ab6e3
